7d9b55300cedfc3d6ea3e63db508001d46c5c955
Алексей <aleksey.galatsan@gmail.com>
2019-08-10 12:35:19 +0300

    Revert "test"
    This reverts commit b8f437d2a5c82a26a91c2768f85458ba02466b26.

--- Messenger/ClientApp/App.jsx
+++ Messenger/ClientApp/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import Messanger from './containers/messanger';
+
 class App extends Component {
   render() {
     return (
@@ -9,4 +11,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
